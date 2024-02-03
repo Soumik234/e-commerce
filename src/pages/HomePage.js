13f6152b5c21ec31f41d36c7a876e3da026a9b1e
Soumik234 <115613165+Soumik234@users.noreply.github.com>
2024-02-03 19:52:14 +0530

    change

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,12 +1,10 @@
 import React from "react";
-import { FeaturedProducts, Hero, Services, Contact } from "../components";
+import { FeaturedProducts, Hero} from "../components";
 const HomePage = () => {
   return (
     <main>
       <Hero />
       <FeaturedProducts />
-      <Services />
-      <Contact />
     </main>
   );
 };
